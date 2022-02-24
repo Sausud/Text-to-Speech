837ddc05e4b5a8c0eff50f3c0cae8c540dca590a
Sausud <100353425+Sausud@users.noreply.github.com>
2022-02-24 17:04:44 +0530

    Add files via upload

---
+++ tts1.py
@@ -0,0 +1,20 @@
+import re
+
+from pygame import mixer
+from gtts import gTTS
+import os
+import sys
+import msvcrt
+text_en = input("write here whatever you want to make system speak : ")
+list_in =  re.split(r"[-;,.\s]\s*",text_en)
+language = 'en'
+len_in= len(list_in)
+for i in range(len_in):
+  myobj = gTTS(text=list_in[i], lang=language, slow=False)
+  myobj.save("v"+str(i)+".mp3")
+  i=i+1
+
+#os.system("start v1.mp3")
+#mixer.init()
+#mixer.music.load("first.mp3")
+#mixer.music.play()
